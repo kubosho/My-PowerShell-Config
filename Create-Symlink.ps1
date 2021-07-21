c0fa17432b8191dac7796befee21f58093bf6852
Shota Kubota <ta2@o2p.jp>
2021-07-21 15:49:30 +0900

    feat(Create-Symlink): add PowerShell profile symlink create script

---
+++ Create-Symlink.ps1
@@ -0,0 +1,5 @@
+$Name = "Microsoft.PowerShell_profile.ps1"
+$Value = "C:\Users\kubosho\src\github.com\kubosho\My-PowerShell-Config\Microsoft.PowerShell_profile.ps1"
+$Path = "C:\Users\kubosho\OneDrive\ドキュメント\PowerShell"
+
+sudo New-Item -ItemType SymbolicLink -Name $Name -Value $Value -Path $Path
